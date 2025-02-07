4d504e2ba8ba7221feff5e726afe7d157a598d3c
Olya74 <olyayoga74@gmail.com>
2025-02-07 16:24:48 +0100

    layout

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a>Kontact</a>
+        </li>
+        <li>
+          <a>Login</a>
+        </li>
+        <li>
+          <a>Logout</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav
---
+++ src/components/ProductsList/ProductsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductsList() {
+  return (
+    <div className='ProductList'>
+      
+    </div>
+  )
+}
+
+export default ProductList
